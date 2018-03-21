90c301ceb2eaf409dea15aa0cdb6f97c40a5ebfc
Oktawian-L <oktawianlasek@gmail.com>
2018-03-21 19:14:00 +0100

    Basics of Bhandari alg

---
+++ Bhandari.m
@@ -0,0 +1,14 @@
+% Simple implementation of Bhandari algoritm
+% INPUTs: graph structure 
+%         s - start node 
+%         t - end node
+%         k - number of paths
+%OUTPUT:
+%    edge-disjoint paths matrix
+
+function T=Bhandari(graph,s,t,k)
+%Find th shortest path Pi from node s to node t
+for i = 1:k
+  T(i)= i*i % example only
+  end
+ end 
