6f3409233c8dd502868cab2b479e5d0ed1c89d72
Oktawian-L <oktawianlasek@gmail.com>
2018-04-22 15:45:02 +0200

    Adding scipt file for another project

---
+++ Matma_P/R_OL.R
@@ -0,0 +1,9 @@
+#remember to change path
+UsedCars2 <- read.csv("C:/GiT/UsedCars.csv");
+#install.packages("recommenderlab") if not exists
+library(recommenderlab);
+UsedCars <- as(UsedCars2, "binaryRatingMatrix"); 
+#diving dataset into 90% for training and 10% for testing
+e <-"evaluationScheme"(UsedCars, method="split",  train=0.9, k=NULL, given =0, goodRating = NA)
+
+
