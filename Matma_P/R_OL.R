b46283e404f5bde100591d6cf397e8f47f3089ef
Oktawian-L <oktawianlasek@gmail.com>
2018-05-07 23:06:59 +0200

    Discretize the input data

--- Matma_P/R_OL.R
+++ Matma_P/R_OL.R
@@ -1,12 +1,27 @@
 #remember to change path
-UsedCars2 <- read.csv("C:/GiT/UsedCars.csv");
+UsedCars <- read.csv("C:/GiT/UsedCars.csv");
 #install.packages("recommenderlab") if not exists
 library(recommenderlab);
+#install.packages("mlbench");
+library(mlbench);
 UsedCars <- as(UsedCars2, "binaryRatingMatrix"); 
-#diving dataset into 90% for training and 10% for testing
-e <-"evaluationScheme"(UsedCars, method="split",  train=0.9, k=NULL, given =0, goodRating = NA)
+rg <- runif(nrow(UsedCars));
+# podzial zbiorow na treningowy i walidacyjny
+g.train <- UsedCars[rg>=0.33,]
+g.valid <- UsedCars[rg<0.33,]
+#dyskrettyzacja wg trzech zbiorow X>9000, 9000<x<11000, 11000<X
+Cena = cut(UsedCars2$Price, br = c(0,9000,11000,99999), labels = c("cheap","mid","expensive"))
+#ile rekordkow wpadlo gdzie
+table(Cena)
+# obliczamy przedzia³y na zbiorze trenuj¹cym
+#disc.mentr.compute(g.train, 1:10)
+#g.train.dmen <- disc.mentr.apply(g.train, 1:10, g.dmen$nparti, g.dmen$points)
+#e <-"evaluationScheme"(UsedCars, method="split",  train=0.7, k=NULL, given =0, goodRating = NA)
+#t <-"evaluationScheme"(UsedCars, method="split",  train=0.3, k=NULL, given =0, goodRating = NA)
+
+#g.train.dmen <- disc.mentr.apply(g.train, 1:10, g.dmen$nparti, g.dmen$points)
 
 
-install.packages("sqldf")
-> gd$mStart <- as.Date(gd$mStart, format="%d/%m/%Y")
-> gd$sStart <- as.Date(gd$sStart, format="%d/%m/%Y")
+#install.packages("sqldf")
+#> gd$mStart <- as.Date(gd$mStart, format="%d/%m/%Y")
+#> gd$sStart <- as.Date(gd$sStart, format="%d/%m/%Y")
